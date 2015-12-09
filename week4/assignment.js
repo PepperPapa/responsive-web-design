51b53fb7a131c779dd00a8a7d8d90b0aaa60d152
pepperpapa <zhongxin2506@outlook.com>
2015-12-10 00:14:09 +0800

    add photos-template

--- week4/assignment.js
+++ week4/assignment.js
@@ -1,14 +1,17 @@
 /*
 *
 */
-var albums_template;
+var albums_template, photos_template;
+
+//variables to store the current displayed album and photo
+var current_album = animals_data.category[0];
 
 // a helper function that instantiates a template
 // and displays the results in the content div
-function showTemplate(template, data){
+function showTemplate(template, data, content){
 	var html    = template(data);
 	console.log(html);
-	$('#content').html(html);
+	$(content).html(html);
 }
 
 // document read gets called when the whole document
@@ -21,6 +24,29 @@ $(document).ready(function(){
 	//
 	var source   = $("#albums-template").html();
 	albums_template = Handlebars.compile(source);
-	console.log(source);
-	showTemplate(albums_template, animals_data);
+
+	source = $("#photos-template").html();
+	photos_template = Handlebars.compile(source);
+
+	// when pages ready show all the albums
+	showTemplate(albums_template, animals_data, '#content');
+	// when pages ready show all photos of current album
+	showTemplate(photos_template, current_album, '#photo-blocks');
+
+	// click one album, show all photos of the album
+	$(".album-thumbnail").click(function (){
+		// get the index (position in the array)
+		// of the album we clicked on
+		// "this" is the element that was clicked on
+		// data("id") gets the attribute data-id
+		// (which we set to the index of the album in
+		// the array - @index)
+		var index = $(this).data("id");
+
+		// set the current album to this album
+		current_album = animals_data.category[index];
+
+		//display the photos template
+		showTemplate(photos_template, current_album, '#photo-blocks');
+	});
 });
