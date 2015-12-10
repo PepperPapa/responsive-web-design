8eab7d5b9777d2c78d1c498321ebf9c20ecb98af
pepperpapa <zhongxin2506@outlook.com>
2015-12-11 00:06:07 +0800

    add search function for week4 assignment

--- week4/assignment.js
+++ week4/assignment.js
@@ -5,12 +5,16 @@ var albums_template, photos_template;
 
 //variables to store the current displayed album and photo
 var current_album = animals_data.category[0];
+var active_album = 0;
 
 // a helper function that instantiates a template
 // and displays the results in the content div
 function showTemplate(template, data, content){
-	var html    = template(data);
-	console.log(html);
+	var html = template(data);
+
+	// this is for debugging
+	// console.log(html);
+
 	$(content).html(html);
 }
 
@@ -42,6 +46,7 @@ $(document).ready(function(){
 		// (which we set to the index of the album in
 		// the array - @index)
 		var index = $(this).data("id");
+		active_album = index;
 
 		// set the current album to this album
 		current_album = animals_data.category[index];
@@ -49,4 +54,53 @@ $(document).ready(function(){
 		//display the photos template
 		showTemplate(photos_template, current_album, '#photo-blocks');
 	});
+
+	  // the search functionality
+    // this happens when a key is pressed
+    // inside the search box
+    $('#search_input').keypress(function (e) {
+
+      // check if the key that was pressed
+      // is the return key (it has id 13)
+      // and only do the search if it is
+      if (e.which == 13) {
+
+        // get the search text which is the
+        // contents of the search box
+        var search_text = $('#search_input').val();
+
+        // print the search box
+        // (this is an example of using
+        // console.log for debugging)
+        console.log(search_text);
+				// create a new array of data with only
+			 // the data that contains the search string
+			 var filteredData = {
+
+				 // use the filter function which returns
+				 // a new array that contains only the
+				 // elements of data.images for which
+				 // the function returns true
+				 animals: animals_data.category[active_album].animals.filter(function(d){
+
+					 // return true if the title contains
+					 // the search text
+					 if (d.name.search(search_text) > -1){
+						 return true;
+					 }
+
+					 // if we reach here it means we haven't
+					 // found a match so return false
+					 return false;
+				 })
+			 	};
+
+			 	// this is for debugging
+			 	console.log(filteredData);
+
+			 	// pass the newly filtered data into
+	      // the template to generate new html
+	      showTemplate(photos_template, filteredData, '#photo-blocks');
+			}
+		});
 });
